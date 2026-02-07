327b97960333789f81a8fc886e9c125eef4b0f17
Alvar Aasgaard <amaasgaa@stud.ntnu.no>
2026-02-07 11:49:14 +0100

    Added the ReplaceTextCommand class, with methods for getTarget, getReplacement and an overriding method for executing the tasks related to the class.

---
+++ src/main/java/org/example/ReplaceTextCommand.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class ReplaceTextCommand implements TextCommand {
+  protected String target;
+  protected String replacement;
+  public ReplaceTextCommand(String target, String replacement) {
+    this.target = target;
+    this.replacement = replacement;
+  }
+
+  public String getTarget() {
+    return target;
+  }
+  public String getReplacement() {
+    return replacement;
+  }
+
+  @Override
+  public String execute(String text) {
+    return text.replace(target, replacement);
+  }
+}
--- src/main/java/org/example/TextCommand.java
+++ src/main/java/org/example/TextCommand.java
@@ -1,7 +1,5 @@
 package org.example;
 
 public interface TextCommand {
-  public String execute(String text){
-    return "";
-  }
+  public String execute(String text);
 }
