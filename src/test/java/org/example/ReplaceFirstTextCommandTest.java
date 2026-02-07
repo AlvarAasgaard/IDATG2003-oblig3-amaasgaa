993477b3d5edbf1132d4b46dd6d17c5109d34ff9
Alvar Aasgaard <amaasgaa@stud.ntnu.no>
2026-02-07 13:18:44 +0100

    Added the test class for the ReplaceTextCommandTest class so that i will be able to test it without actually running the script.

---
+++ src/test/java/org/example/ReplaceFirstTextCommandTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReplaceFirstTextCommandTest {
+  @Test
+  void testReplaceFirstText() {
+    TextCommand command = new ReplaceFirstTextCommand("is", "was");
+    String input = "Here is a test where there is alot of words";
+
+    String result = command.execute(input);
+
+    assertEquals("Here was a test where there is alot of words", result);
+  }
+
+  @Test
+  void testExecuteWithEmptyString() {
+    TextCommand command = new ReplaceFirstTextCommand("is", "was");
+    assertEquals("", command.execute(""));
+  }
+}
