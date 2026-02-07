153c70a65db4d752edd9749639734dedb240d81b
Alvar Aasgaard <amaasgaa@stud.ntnu.no>
2026-02-07 12:24:21 +0100

    Created the ReplaceFirstTextCommand class, with an execute method that overrides the execute command from the ReplaceTextCommand class.

---
+++ src/main/java/org/example/ReplaceFirstTextCommand.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class ReplaceFirstTextCommand extends ReplaceTextCommand{
+  public ReplaceFirstTextCommand(String target, String replacement) {
+    super(target, replacement);
+    }
+
+  @Override
+  public String execute(String text) {
+    if (text == null) {
+      return null;
+    }
+    return text.replaceFirst(this.target, this.replacement);
+  }
+}
