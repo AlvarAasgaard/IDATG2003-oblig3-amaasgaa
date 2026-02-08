721d82ed2a11ffe99fd10be43044b4c0c6213e39
Alvar Aasgaard <amaasgaa@stud.ntnu.no>
2026-02-08 13:33:27 +0100

    Added the CapitalizeWordTextCommand class with JavaDoc.

---
+++ src/main/java/org/example/capitalizeText/CapitalizeWordTextCommand.java
@@ -0,0 +1,53 @@
+package org.example.capitalizeText;
+
+/**
+ * This class represents a command that capitalizes the
+ * first letters of all the words in a given text string.
+ * <p>
+ *   It extends the {@link CapitalizeTextCommand} to provide specific logic.
+ * </p>
+ */
+public class CapitalizeWordTextCommand extends CapitalizeTextCommand{
+  /**
+   * Constructs a new CapitalizeWordTextCommand
+   * @param text the text to capitalize each word in.
+   */
+  public CapitalizeWordTextCommand(String text){
+    super(text);
+  }
+
+  /**
+   * The execution of the command to capitalize each word.
+   * <p>
+   *   It checks if the input text is empty or null, then splits the
+   *   string into words based on spaces. Each word is transformed by making the first character
+   *   uppercase and the subsequent characters lowercase.
+   * </p>
+   * @param text the text string to be capitalized.
+   * @return the proceeded string with each word capitalized
+   * or an error message if invalid input.
+   */
+  @Override
+  public String execute(String text){
+    if (text == null || text.isEmpty()){
+      return "No text given.";
+    }
+    String[] words = text.split(" ");
+    StringBuilder result = new StringBuilder();
+
+    for (int i=0; i<words.length; i++){
+      String word = words[i];
+
+      if (!word.isEmpty()){
+        String capitalized = word.substring(0, 1).toUpperCase()
+            + word.substring(1).toLowerCase();
+        result.append(capitalized);
+      }
+      if (i+1 < words.length-1){
+        result.append(" ");
+      }
+    }
+    return result.toString();
+  }
+
+}
