f755bb0e4a2f7ca951f05ea22ff6dec4ba9eba5d
Alvar Aasgaard <amaasgaa@stud.ntnu.no>
2026-02-09 08:48:36 +0100

    Renamed the Main class to Script and added simple logic for it to work as expected.

---
+++ src/main/java/org/example/Script.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import java.util.List;
+
+public class Script {
+  private List<TextCommand> commands;
+
+  public Script(List<TextCommand> commands){
+    this.commands = commands;
+  }
+
+  public String execute(String text){
+    String result = text;
+
+    for (TextCommand command : commands){
+      result = command.execute(result);
+    }
+    return result;
+  }
+}
